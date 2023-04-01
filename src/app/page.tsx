7f1030a632b50584cd299ca8bd6ac87c87c2dab4
f1lander <filanderuclez@gmail.com>
2023-03-31 21:11:50 -0600

    adds workflow to deploy to gh-pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const FEATURES_FLAGS = {
+  SHOW_DOCS: false,
+  SHOW_TEMPLATES: false,
+  SHOW_DEPLOY: true,
+} as const;
+
+type FeatureFlagProps = {
+  name: keyof typeof FEATURES_FLAGS;
+  children: React.ReactNode;
+};
+
+const isFeatureFlag = (
+  name: keyof typeof FEATURES_FLAGS
+): name is keyof typeof FEATURES_FLAGS => {
+  return FEATURES_FLAGS[name];
+};
+
+const FeatureFlag: React.FC<FeatureFlagProps> = ({ name, children }) => {
+  if (FEATURES_FLAGS[name]) {
+    return <>{children}</>;
+  }
+
+  return null;
+};
 
 export default function Home() {
   return (
@@ -18,7 +43,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -46,32 +71,36 @@ export default function Home() {
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
+        <FeatureFlag name="SHOW_DOCS">
+          <a
+            href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
+        </FeatureFlag>
+        {isFeatureFlag("SHOW_TEMPLATES") && (
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Templates <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Explore the Next.js 13 playground.
+            </p>
+          </a>
+        )}
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
@@ -87,5 +116,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
